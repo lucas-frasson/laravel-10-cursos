38f4b951da79a185d73aff7d26c4edd3d64f8dcb
lucas-frasson <lgfrasson36@gmail.com>
2024-03-02 01:56:09 -0300

    Auth Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\CursoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/cursos', CursoController::class);
-// Route::delete('/cursos/{id}', [CursoController::class, 'destroy']);
-// Route::patch('/cursos/{id}', [CursoController::class, 'update']);
-// Route::get('/cursos/{id}', [CursoController::class, 'show']);
-// Route::get('/cursos', [CursoController::class, 'index']);
-// Route::post('/cursos', [CursoController::class, 'store']);
+// Rota de login
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+// Rota de cursos
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::apiResource('/cursos', CursoController::class);
+    // Route::delete('/cursos/{id}', [CursoController::class, 'destroy']);
+    // Route::patch('/cursos/{id}', [CursoController::class, 'update']);
+    // Route::get('/cursos/{id}', [CursoController::class, 'show']);
+    // Route::get('/cursos', [CursoController::class, 'index']);
+    // Route::post('/cursos', [CursoController::class, 'store']); 
+});
+
+// Rota de teste
 Route::get('/', function () {
     return response()->json([
         'success' => true
