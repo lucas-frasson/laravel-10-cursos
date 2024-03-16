3e196b4555d91645e6ee5dfe170a6a4d218b1ac2
lucas-frasson <lgfrasson36@gmail.com>
2024-03-15 22:33:10 -0300

    Seed para criar user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+        if(!User::where('name', 'Teste')->first()){
+            User::create([
+                'name' => 'Teste',
+                'email' => 'teste@email.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+    }
+}
